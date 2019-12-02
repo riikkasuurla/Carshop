949992764f395a864efa4889bb13f258306f1349
riikkasuurla <riikka@suurla.fi>
2019-12-02 17:36:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Carlist from './Components/Carlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <div className="App">
+       <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            CarShop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Carlist />
+    </div>
+  );
+}
+
+export default App;
